b8d2599f78f626f10f56460bd2fe4b6b86d0653d
sourav-mahmud <souravmahmud32@gmail.com>
2022-11-28 22:03:32 +0600

    delete method added  added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -20,6 +21,7 @@ async function run(){
         const brandsCollection = client.db('carGarage').collection('brandsCollection');
         const individualBrandCollection = client.db('carGarage').collection('individualBrand');
         const bookingsCollection = client.db('carGarage').collection('bookings');
+        const usersCollection = client.db('carGarage').collection('users');
 
         app.get('/brandsCollection', async(req, res) =>{
             const query = {};
@@ -34,12 +36,59 @@ async function run(){
             res.send(options);
         });
 
+        app.get('/bookings', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async(req, res) =>{
             const booking = req.body
             console.log(booking);
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
-        })
+        });
+
+        app.get("/jwt", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+              const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+                expiresIn: "1h",
+              });
+              res.send({ accessToken: token });
+            }
+            // res.status(403).send({accessToken: ''})
+          });
+
+          app.get("/users", async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+          });
+
+          app.get("/users/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === "admin" });
+          });
+        
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result); 
+        });
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await usersCollection.findOneAndDelete(query);
+            console.log(result);
+            res.send(result);
+        });
     }
     finally{}
 }
